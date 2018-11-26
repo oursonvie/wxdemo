c242dbc133c2f19d796519082241c46c9ff7167f
Tonny <oursonvie@qq.com>
2018-11-26 09:58:03 +0800

    parser xml body

--- server/main.js
+++ server/main.js
@@ -1,6 +1,24 @@
+// add body praser
+var bodyParser = require('body-parser')
+require('body-parser-xml')(bodyParser);
+
+Picker.middleware( bodyParser.xml() );
+
+
+
 // route for wx validation
 Picker.route('/callback', function(params, req, res, next) {
-  var body = params.query;
+  let body = params.query;
+  console.log(`[Request Methods] ${req.method}`)
+  // console.log(req.headers)
+
+  // xml body
+  if (req.body && req.body.xml) {
+    let bodyContent = req.body.xml
+    console.log(bodyContent)
+  }
+
+
 
   if ( valideWX(body.nonce, body.timestamp, body.signature) ) {
     res.end(body.echostr)
