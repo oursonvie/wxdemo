da1b2a7943b414c26748ce6ad1eb4c67a8263892
Tonny <yixiongzhang@Yixiongs-MBP.lan>
2019-02-01 19:06:08 +0800

    transfer chat to customer service

--- server/wx/customFunctions/keywordReplay.js
+++ server/wx/customFunctions/keywordReplay.js
@@ -12,6 +12,23 @@ keywordReplay = (bodyContent) => {
 
     return replay
 
+  } else if ( bodyContent.Content[0].includes('转接客服') ) {
+
+    // need to check account before transfor customservice
+    console.log(`转接${bodyContent.FromUserName}，专属客服`)
+
+    replay = {
+      "ToUserName": bodyContent.FromUserName,
+      "FromUserName": bodyContent.ToUserName,
+      "CreateTime": moment().unix(),
+      "MsgType": "transfer_customer_service",
+      "TransInfo": {
+        KfAccount: "kf2001@xdrdedu"
+      }
+    }
+
+    return replay
+
   } else {
     return false
   }
--- server/wx/customServiceFunctions/basicFunctions.js
+++ server/wx/customServiceFunctions/basicFunctions.js
@@ -1,12 +1,10 @@
 // get customer service ID
 
 getCurrentCustomerServicesList = () => {
-  PromiseHTTPCall('GET', `https://api.weixin.qq.com/cgi-bin/customservice/getkflist?access_token=${accessTokens()}`)
-  .then( res => {
-    console.log(res.content)
-  })
-  .catch(err => {
+  try {
+    res = Promise.await(PromiseHTTPCall('GET', `https://api.weixin.qq.com/cgi-bin/customservice/getkflist?access_token=${accessTokens()}`))
+    return res.data
+  } catch (err) {
     console.log(err)
-  })
-  return
+  }
 }
