8309c7f58a9fb74ecd4f033bb6fdd0b633c68d22
Tonny <yixiongzhang@Yixiongs-MBP.lan>
2019-02-01 20:12:05 +0800

    working on admin view

--- libs/route.js
+++ libs/route.js
@@ -4,3 +4,10 @@ FlowRouter.route('/', {
         BlazeLayout.render('MainLayout', {main: 'homePage'});
     }
 });
+
+FlowRouter.route('/admin', {
+   name: 'adminHome',
+    action() {
+        BlazeLayout.render('desktopLayout', {main: 'adminHome'});
+    }
+});
